7a5f859a0f09f8f6bfa3df5cd0436a78715da5b3
haileeyun <121257829+haileeyun@users.noreply.github.com>
2025-02-09 23:12:14 -0500

    added my own 2 sprites and added simple movement

--- main.cpp
+++ main.cpp
@@ -8,9 +8,14 @@
 * Academic Misconduct.
 **/
 
+// note to self: have 2 sprites of my own art, rotating on the z-axis.
+// successfully runs
+
 
 #define GL_SILENCE_DEPRECATION
 #define GL_GLEXT_PROTOTYPES 1
+#define LOG(argument) std::cout << argument << '\n'
+
 
 #ifdef _WINDOWS
 #include <GL/glew.h>
@@ -21,6 +26,9 @@
 #include "glm/mat4x4.hpp"                // 4x4 Matrix
 #include "glm/gtc/matrix_transform.hpp"  // Matrix transformation methods
 #include "ShaderProgram.h"               
+
+#define STB_IMAGE_IMPLEMENTATION // to load my own images
+#include "stb_image.h"
 
 enum AppStatus { RUNNING, TERMINATED };
 
@@ -34,17 +42,33 @@ BG_BLUE = 0.549f,
 BG_GREEN = 0.9059f,
 BG_OPACITY = 1.0f;
 
+// shaders
+constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
+               F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
+
+
+// texture constants
+constexpr GLint NUMBER_OF_TEXTURES = 1, // to be generated, that is
+                LEVEL_OF_DETAIL    = 0, // mipmap reduction image level
+                TEXTURE_BORDER     = 0; // this value MUST be zero
+
+constexpr char BLACK_CAT_SPRITE_FILEPATH[]   = "shaders/black_cat.png",
+               BUTTERFLY_SPRITE_FILEPATH[] = "shaders/butterfly.png";
+
+// initial position of sprites
+constexpr glm::vec3 INIT_SCALE      = glm::vec3(5.0f, 5.98f, 0.0f),
+                    INIT_POS_BLACK_CAT   = glm::vec3(2.0f, 0.0f, 0.0f),
+                    INIT_POS_BUTTERFLY = glm::vec3(-2.0f, 0.0f, 0.0f);
+
+constexpr float ROT_INCREMENT = 1.0f; // rotational constant
+
+
 // Our viewport—or our "camera"'s—position and dimensions
 constexpr int VIEWPORT_X = 0,
 VIEWPORT_Y = 0,
 VIEWPORT_WIDTH = WINDOW_WIDTH,
 VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
-// Our shader filepaths; these are necessary for a number of things
-// Not least, to actually draw our shapes 
-// We'll have a whole lecture on these later
-constexpr char V_SHADER_PATH[] = "shaders/vertex.glsl",
-F_SHADER_PATH[] = "shaders/fragment.glsl";
 
 // Our object's fill colour
 constexpr float TRIANGLE_RED = 1.0,
@@ -58,6 +82,8 @@ SDL_Window* g_display_window;
 ShaderProgram g_shader_program;
 
 glm::mat4 g_view_matrix,        // Defines the position (location and orientation) of the camera
+g_black_cat_matrix,
+g_butterfly_matrix,
 g_model_matrix,       // Defines every translation, rotation, and/or scaling applied to an object; we'll look at these next week
 g_projection_matrix;  // Defines the characteristics of your camera, such as clip panes, field of view, projection method, etc.
 
@@ -67,13 +93,51 @@ float g_previous_ticks;
 float g_radius = 2;
 float g_frames = 0;
 
+glm::vec3 g_rotation_black_cat   = glm::vec3(0.0f, 0.0f, 0.0f),
+          g_rotation_butterfly = glm::vec3(0.0f, 0.0f, 0.0f);
+
+GLuint g_black_cat_texture_id,
+       g_butterfly_texture_id;
+
+GLuint load_texture(const char* filepath)
+{
+    // STEP 1: Loading the image file
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
+    
+    if (image == NULL)
+    {
+        LOG("Unable to load image. Make sure the path is correct.");
+        assert(false);
+    }
+    
+    // STEP 2: Generating and binding a texture ID to our image
+    GLuint textureID;
+    glGenTextures(NUMBER_OF_TEXTURES, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    
+    // STEP 3: Setting our texture filter parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    
+    // STEP 4: Releasing our file from memory and returning our texture id
+    stbi_image_free(image);
+    
+    return textureID;
+}
+
+
 void initialise()
 {
-    SDL_Init(SDL_INIT_VIDEO);
-    g_display_window = SDL_CreateWindow("Hello, Triangle!",
+    SDL_Init(SDL_INIT_VIDEO);  // initialize video
+
+    g_display_window = SDL_CreateWindow("Hello, textures!",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH, WINDOW_HEIGHT,
         SDL_WINDOW_OPENGL);
+
+      
 
     if (g_display_window == nullptr)
     {
@@ -97,21 +161,26 @@ void initialise()
     // Load up our shaders
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
 
-    // Initialise our view, model, and projection matrices
-    g_view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
-    g_model_matrix = glm::mat4(1.0f);  // Defines every translation, rotations, or scaling applied to an object
-    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
+    g_black_cat_matrix       = glm::mat4(1.0f);
+    g_butterfly_matrix     = glm::mat4(1.0f);
+    g_view_matrix       = glm::mat4(1.0f);
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
+    //g_model_matrix = glm::mat4(1.0f);  // Defines every translation, rotations, or scaling applied to an object
 
     g_shader_program.set_projection_matrix(g_projection_matrix);
     g_shader_program.set_view_matrix(g_view_matrix);
-    // Notice we haven't set our model matrix yet!
 
-    g_shader_program.set_colour(TRIANGLE_RED, TRIANGLE_BLUE, TRIANGLE_GREEN, TRIANGLE_OPACITY);
-
-    // Each object has its own unique ID
     glUseProgram(g_shader_program.get_program_id());
 
     glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+
+    g_black_cat_texture_id   = load_texture(BLACK_CAT_SPRITE_FILEPATH);
+    g_butterfly_texture_id = load_texture(BUTTERFLY_SPRITE_FILEPATH);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 }
 
 void process_input()
@@ -126,45 +195,80 @@ void process_input()
     }
 }
 
-void update() {
-    float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND; // current # of ticks
-    float delta_time = ticks - g_previous_ticks; // tick difference from the last frame
+void update()
+{
+    /* Delta time calculations */
+    float ticks = (float) SDL_GetTicks() / MILLISECONDS_IN_SECOND;
+    float delta_time = ticks - g_previous_ticks;
     g_previous_ticks = ticks;
-    // g_frames++; // don't need frames anymore
+    
+    /* Game logic */
+    g_rotation_black_cat.y += ROT_INCREMENT * delta_time;
+    g_rotation_butterfly.y += -1 * ROT_INCREMENT * delta_time;
 
-    static float rotation_angle = 0.0f;
-    float rotation_speed = 1.0f;
-    rotation_angle += rotation_speed * delta_time; // account for frame speed
-
-    g_position.x = g_radius * cos(rotation_angle);
-    g_position.y = g_radius * sin(rotation_angle);
-
-    /* Reset the Model Matrix */
-    g_model_matrix = glm::mat4(1.0f);
-
-    /* Translate */
-    g_model_matrix = glm::translate(g_model_matrix, g_position);
+    /* Model matrix reset */
+    g_black_cat_matrix   = glm::mat4(1.0f);
+    g_butterfly_matrix = glm::mat4(1.0f);
+    
+    /* Transformations */
+    g_black_cat_matrix = glm::translate(g_black_cat_matrix, INIT_POS_BLACK_CAT);
+    g_black_cat_matrix = glm::rotate(g_black_cat_matrix,
+                                 g_rotation_black_cat.y,
+                                 glm::vec3(0.0f, 1.0f, 0.0f));
+    g_black_cat_matrix = glm::scale(g_black_cat_matrix, INIT_SCALE);
+    
+    g_butterfly_matrix = glm::translate(g_butterfly_matrix, INIT_POS_BUTTERFLY);
+    g_butterfly_matrix = glm::rotate(g_butterfly_matrix,
+                                  g_rotation_butterfly.y,
+                                  glm::vec3(0.0f, 1.0f, 0.0f));
+    g_butterfly_matrix = glm::scale(g_butterfly_matrix, INIT_SCALE);
 }
 
-void render() {
+void draw_object(glm::mat4 &object_g_model_matrix, GLuint &object_texture_id)
+{
+    g_shader_program.set_model_matrix(object_g_model_matrix);
+    glBindTexture(GL_TEXTURE_2D, object_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so use 6, not 3
+}
+
+
+void render()
+{
     glClear(GL_COLOR_BUFFER_BIT);
-
-    g_shader_program.set_model_matrix(g_model_matrix);
-
+    
+    // Vertices
     float vertices[] =
     {
-         0.5f, -0.5f,
-         0.0f,  0.5f,
-        -0.5f, -0.5f
+        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
     };
 
-    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
+    // Textures
+    float texture_coordinates[] =
+    {
+        0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
+        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
+    };
+    
+    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false,
+                          0, vertices);
     glEnableVertexAttribArray(g_shader_program.get_position_attribute());
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    
+    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT,
+                          false, 0, texture_coordinates);
+    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+    
+    // Bind texture
+    draw_object(g_black_cat_matrix, g_black_cat_texture_id);
+    draw_object(g_butterfly_matrix, g_butterfly_texture_id);
+    
+    // We disable two attribute arrays now
     glDisableVertexAttribArray(g_shader_program.get_position_attribute());
-
+    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+    
     SDL_GL_SwapWindow(g_display_window);
 }
+
 
 void shutdown() { SDL_Quit(); }
 
